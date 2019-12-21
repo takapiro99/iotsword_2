ddb64facd1251a41b5806ea3ed9a1cc89c8e27e8
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2019-12-22 00:30:56 +0900

    for something

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const server = require('../bin/www');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('control', { title: 'Express' });
+  res.render('u_drawing', { title: 'Express' });
 });
 
 
