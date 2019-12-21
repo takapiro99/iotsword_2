b1cd5322284b0d1d8be2197cf1a60bb93ca9a9d4
takapiro99 <55702777+takapiro99@users.noreply.github.com>
2019-12-22 00:28:03 +0900

    made awsome color picker UI

--- routes/index.js
+++ routes/index.js
@@ -3,38 +3,15 @@ var router = express.Router();
 var socket = require("socket.io");
 const server = require('../bin/www');
 
-//var io = socket(server);
-var io = require('socket.io').listen(server);
-
-
-io.on("connection",function(socket){
-  console.log("made socket connection",socket.id);
-
-  socket.on("chat",function(data){
-      io.sockets.emit("chat",data);
-      console.log(data);
-  });
-
-  socket.on("typing",function(data){
-      socket.broadcast.emit("typing",data)
-  })
-}); 
-
-
-
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
   res.render('control', { title: 'Express' });
 });
 
 
 router.get('/me', function(req, res, next) {
-
   res.render('me', { title: 'this is my page' });
 });
-
-
 
 module.exports = router;
