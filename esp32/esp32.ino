21eeb20580d2ee6b33181faf066ec6c160094a79
takapiro99 <bigstitch99@gmail.com>
2020-11-20 13:21:10 +0900

    refactor and format

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,31 +1,18 @@
 /*
- Basic ESP8266 MQTT example
-
- This sketch demonstrates the capabilities of the pubsub library in combination
- with the ESP8266 board/library.
-
- It connects to an MQTT server then:
-  - publishes "hello world" to the topic "outTopic" every two seconds
-  - subscribes to the topic "inTopic", printing out any messages
-    it receives. NB - it assumes the received payloads are strings not binary
-  - If the first character of the topic "inTopic" is an 1, switch ON the ESP Led,
-    else switch it off
-
- It will reconnect to the server if the connection is lost using a blocking
- reconnect function. See the 'mqtt_reconnect_nonblocking' example for how to
- achieve the same result without blocking the main loop.
-
- To install the ESP8266 board, (using Arduino 1.6.4+):
-  - Add the following 3rd party board manager under "File -> Preferences -> Additional Boards Manager URLs":
-       http://arduino.esp8266.com/stable/package_esp8266com_index.json
-  - Open the "Tools -> Board -> Board Manager" and click install for the ESP8266"
-  - Select your ESP8266 in "Tools -> Board"
-
+  iotsword_2
 */
 
-#include <WiFi.h>
-#include <PubSubClient.h>
 #include <FastLED.h>
+#include <PubSubClient.h>
+#include <WiFi.h>
+
+// Some for LEDs
+#define LED_PIN 13
+#define NUM_LEDS 28
+#define BRIGHTNESS 150
+#define LED_TYPE WS2812B
+#define COLOR_ORDER GRB
+#define UPDATES_PER_SECOND 65
 
 // Update these with values suitable for your network.
 
@@ -33,18 +20,9 @@ const char* ssid = "takapiro2";
 const char* password = "2kumifriends";
 const char* mqtt_server = "153.126.160.230";
 
-
-// Some for LEDs
-#define LED_PIN     13
-#define NUM_LEDS    28
-#define BRIGHTNESS  150
-#define LED_TYPE    WS2812B
-#define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
-#define UPDATES_PER_SECOND 65
 CRGBPalette16 currentPalette;
-TBlendType    currentBlending;
-
+TBlendType currentBlending;
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -54,8 +32,7 @@ int value = 0;
 
 void setup_wifi() {
   delay(10);
-  // We start by connecting to a WiFi network
-  Serial.println();
+  Serial.println("");
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
@@ -71,82 +48,72 @@ void setup_wifi() {
 }
 
 void ledsetup() {
-    delay( 2000 ); // power-up safety delay
-    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-    FastLED.setBrightness( BRIGHTNESS );
-    //Palette1();
-    currentBlending = LINEARBLEND;
+  delay(2000);  // power-up safety delay
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS)
+      .setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+  // Palette1();
+  currentBlending = LINEARBLEND;
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
-  //Serial.print(topic);
-  //int i = topic - '0';
-  //Serial.print("%d", topic - '0'); // 1
-
-  if((char)topic[0]=='c'){
-    //Serial.print(topic[6]);
-    //Serial.print(topic[7]);
-    int num = ((int)topic[6]-48)*10 + (int)topic[7]-48;
+  if ((char)topic[0] == 'c') {
+    int num = ((int)topic[6] - 48) * 10 + (int)topic[7] - 48;
+    // chr to int, but there must be a better way
     Serial.print(num);
     Serial.print(": ");
-    //leds[3].r = 105;
-    //leds[3].g = 23;
-    //int wow = (int)topic[6]+2
-    //Serial.print((int)topic[6]);
-    //Serial.print(": ");
-    int r = ((int)payload[0]-48)*100 + ((int)payload[1]-48)*10 + (int)payload[2]-48;
-    int g = ((int)payload[3]-48)*100 + ((int)payload[4]-48)*10 + (int)payload[5]-48;
-    int b = ((int)payload[6]-48)*100 + ((int)payload[7]-48)*10 + (int)payload[8]-48;
-    Serial.print(r);Serial.print(",");Serial.print(g);Serial.print(",");Serial.print(b);
-      Serial.print("] ");
-  Serial.println();
-  leds[num].r=r;leds[num].g=g;leds[num].b=b;
-   FastLED.show();
-    }  
+    int r = ((int)payload[0] - 48) * 100 + ((int)payload[1] - 48) * 10 +
+            (int)payload[2] - 48;
+    int g = ((int)payload[3] - 48) * 100 + ((int)payload[4] - 48) * 10 +
+            (int)payload[5] - 48;
+    int b = ((int)payload[6] - 48) * 100 + ((int)payload[7] - 48) * 10 +
+            (int)payload[8] - 48;
+    Serial.print(r);
+    Serial.print(",");
+    Serial.print(g);
+    Serial.print(",");
+    Serial.print(b);
+    Serial.print("] ");
+    Serial.println();
+    leds[num].r = r;
+    leds[num].g = g;
+    leds[num].b = b;
+    FastLED.show();
+  }
 }
 
 void reconnect() {
-  // Loop until we're reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Create a random client ID
     String clientId = "ESP8266Client-";
     clientId += String(random(0xffff), HEX);
-    // Attempt to connect
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      //client.publish("outTopic", "hello world");
-      // ... and resubscribe
       client.subscribe("inTopic");
       client.subscribe("color/+");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
       Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+      delay(5000);  // Wait 5 seconds before retrying
     }
   }
 }
 
-
-
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
   setup_wifi();
+  ledsetup();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
-  ledsetup();
 }
-
 
 void loop() {
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
-
 }
